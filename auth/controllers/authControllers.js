546712a0b0cc7e0ac2c08d4389bc7bd27ddc0def
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-11-05 11:34:31 -0800

    Add email validation and improve signup error handling
    Introduced the 'validator' package to validate email addresses in the User model. Enhanced error handling in the signup controller to provide clearer feedback for duplicate and validation errors.

--- auth/controllers/authControllers.js
+++ auth/controllers/authControllers.js
@@ -1,18 +1,42 @@
 import User from "../models/User.js";
 
+//handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  //duplicate error code
+  if (err.code === 11000) {
+    errors.email = "that email is already registered";
+    return errors;
+  }
+
+  //validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
+// Login controller
 export const login_post = (req, res) => {
   const { email, password } = req.body;
   console.log(`email: ${email}, password: ${password}`);
   res.send("hello from login");
 };
 
+// Signup controller
 export const signup_post = async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
-    console.error(err);
-    res.status(400).json({ error: err.message });
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
--- auth/models/User.js
+++ auth/models/User.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import validator from "validator";
+const { isEmail } = validator;
 
+// User Schema
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
