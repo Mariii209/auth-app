8bda39622cc645c1d61ac44b9c04a0ed96c9d1d9
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-11-04 22:29:14 -0800

    Add custom validation messages to User schema
    Custom error messages have been added for required and minlength validations on the email and password fields in the User schema to improve feedback during user registration.

--- auth/models/User.js
+++ auth/models/User.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Password must be at least 6 characters long"],
   },
 });
 
